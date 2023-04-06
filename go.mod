75a533f024f12e25db7411dd9a9daf8c64367f64
InYuusha <asuse975@gmail.com>
2023-04-06 07:31:00 +0530

    env

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/InYuusha
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/joho/godotenv v1.5.1 // indirect
+)
 
 go 1.16
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/InYuusha/api"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	port := "5000"
+	port := os.Getenv("PORT")
 	app := gin.Default()
 	api.ApplyRoutes(app)
 	app.Run(":" + port)
